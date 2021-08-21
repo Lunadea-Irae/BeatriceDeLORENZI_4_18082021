f3b5c2a4104451b8dd3e42584c7413a3c5bb214a
Béatrice de LORENZI <lunadea.irae@gmail.com>
2021-08-21 11:56:09 +0200

    success-msg & fail-smg corrections

--- js/formHandler.js
+++ js/formHandler.js
@@ -42,7 +42,7 @@ $(function()
 		 			{  
 						if($form.is('[success-msg]')) // Show Success Message
 						{
-							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('Votre message a bien été envoyé')+"</strong></div></div>");
+							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Votre message a bien été envoyé.</strong></div></div>");
 						}
 						else // Re-Direct
 						{
@@ -55,7 +55,7 @@ $(function()
 			   		{
 						if($('#form-alert').length == 0)
 						{
-							$form.append("<div id='form-alert'><div class='alert alert-danger'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('Il semblerait que le serveur ne réponde pas, nous somme navrée de la gêne occasionnée.')+"</strong></div></div>");
+							$form.append("<div id='form-alert'><div class='alert alert-danger'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>Il semblerait que le serveur ne réponde pas, nous somme navrée de la gêne occasionnée.</strong></div></div>");
 						}	
 			   		},
 		   		});
